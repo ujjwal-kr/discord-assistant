e8a63830b31834bdaad83d8cb2958254692faf66
TheEmperor342 <83999665TheEmperor342@users.noreply.github.com>
2021-05-30 15:46:24 +0530

    Fixed Bugs

--- main.py
+++ main.py
@@ -17,6 +17,7 @@
 
 import discord, json, os
 from discord.ext import commands
+from time import time
 
 #############################################################################################
 
@@ -39,25 +40,18 @@ for i in os.listdir("Cogs"):
         bot.load_extension(F"Cogs.{i[:-3]}")
 
 @bot.event
-async def on_ready(): print("The Bot is Ready")
-
-## You can uncomment this function and put all the id's of users whom you want which will be able to reload cogs
-# @bot.command()
-# async def reload(ctx,cog):
-#     if ctx.author.id in [Uncomment and put your id here if you want to use this function]: # list example -> [<id in integer form>, <another id>]
-#         if cog.lower() == "all":
-#             for i in os.listdir("Cogs"):
-#                 if i.endswith(".py"):
-#                     bot.unload_extension(F"Cogs.{i[:-3]}")
-#                     bot.load_extension(F"Cogs.{i[:-3]}")
-#         await ctx.send("Reloaded all Cogs")
-#     else:
-#         bot.unload_extension(F"Cogs.{cog}")
-#         bot.load_extension(F"Cogs.{cog}")
-# You can uncomment this function and put all the id's of users whom you want which will be able to reload cogs
+async def on_ready(): 
+    print("The Bot is Ready")
+    with open("Configuration/config.json") as f:
+        config = json.loads(f.read())
+        config["starttime"] = float(time())
+        
+    with open("Configuration/config.json", 'w') as f:
+        f.write(json.dumps(config))
+    
 @bot.command()
 async def reload(ctx,cog):
-    if ctx.author.id in [754894159403286531]: # list example -> [<id in integer form>, <another id>]
+    if ctx.author.id in [754894159403286531, 510480545160101898]:
         if cog.lower() == "all":
             for i in os.listdir("Cogs"):
                 if i.endswith(".py"):
