14ca562d3dfbcacec7739906c9c34e661ec6f114
Maanas Nair <63333626+itsmebirdie@users.noreply.github.com>
2021-05-31 20:04:20 +0530

    Create setup.py for installing packages

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+   name='Discord Assistant',
+   version='0.7.1',
+   description='An open source discord bot for everyone!',
+   author='[ᴛʜᴇ ᴇᴍᴘᴇʀᴏʀ]',
+   author_email='staff@hackarmour.tech',
+   install_requires=['discord'],
+)
