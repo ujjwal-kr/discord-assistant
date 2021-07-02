2fe3d0a8c93c07cbc527e34e68140b4a3bae6272
Maanas Nair <63333626+itsmebirdie@users.noreply.github.com>
2021-07-02 18:01:44 +0530

    Undo prefix change

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ TOKEN = input("Enter The Token of your bot: ")
 ## ==> CREATING BOT AND IMPORTING COGS
 #############################################################################################
 
-bot = commands.Bot(command_prefix="!", intents=discord.Intents.all(),case_insensitive=True)
+bot = commands.Bot(command_prefix=">", intents=discord.Intents.all(),case_insensitive=True)
 bot.remove_command("help")
 
 for i in os.listdir("Cogs"):
